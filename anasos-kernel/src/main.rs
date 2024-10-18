1252df9ae58954bd81bcf7c97c147bda62811894
Mrgoblings <mrgoblings@gmail.com>
2024-10-18 11:08:03 +0300

    update: vga support - non-working

--- anasos-kernel/src/main.rs
+++ anasos-kernel/src/main.rs
@@ -2,32 +2,19 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+// use core::fmt::Write;
 
 mod vga;
 
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga::print("AAA Hello!!!!@@");
+    println!("Hello World{}", "!");
     loop {}
 }
 
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
 
-
-/*
-pub fn print(input: &[u8]){
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in input.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    loop {}
 }
-*/
