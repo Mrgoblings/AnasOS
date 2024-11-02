b836bca3121646cad5b18ad3aded98f3c9201c1f
Mrgoblings <mrgoblings@gmail.com>
2024-11-02 20:29:33 +0200

    chore: clean up the build scipt

--- anasos-kernel/build.rs
+++ anasos-kernel/build.rs
@@ -2,13 +2,10 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-    // Get the target directory from Cargo's environment
-    let target_dir = env::var("OUT_DIR").unwrap(); // OUT_DIR is a directory for build artifacts
+    let target_dir = env::var("OUT_DIR").unwrap(); 
 
-    // Define the output path for the bootloader object file
     let bootloader_o_path = format!("{}/bootloader.o", target_dir);
 
-    // Assemble the bootloader
     let status = Command::new("nasm")
         .args(&["-f", "elf64", "boot/bootloader.asm", "-o", &bootloader_o_path])
         .status()
@@ -18,7 +15,7 @@ fn main() {
         panic!("Assembly failed with status: {}", status);
     }
 
-    // Specify custom linker arguments
-    println!("cargo:rustc-link-arg=-Tlinker.ld"); // Use the correct linker script name
-    println!("cargo:rustc-link-arg={}", bootloader_o_path); // Ensure the correct path is used
+    // Custom linker arguments
+    println!("cargo:rustc-link-arg=-Tlinker.ld");
+    println!("cargo:rustc-link-arg={}", bootloader_o_path);
 }
