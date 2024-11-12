477b7cc014568118962aea2da70de696fa552d0a
Mrgoblings <mrgoblings@gmail.com>
2024-11-12 23:25:05 +0200

    chore

--- anasos-kernel/bootloader/header.asm
+++ anasos-kernel/bootloader/header.asm
@@ -1,16 +1,16 @@
 SECTION .multiboot_header
 header_start:
 	; magic number
-	dd 0xe85250d6 ; multiboot2
+	DD 0xe85250d6 ; multiboot2
 	; architecture
-	dd 0 ; protected mode i386
+	DD 0 ; protected mode i386
 	; header length
-	dd header_end - header_start
+	DD header_end - header_start
 	; checksum
-	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+	DD 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
 	; end tag
-	dw 0
-	dw 0
-	dd 8
+	DW 0
+	DW 0
+	DD 8
 header_end:
