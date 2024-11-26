d50052ab2c12481908ad1fdde0c7edbea5833061
Mrgoblings <mrgoblings@gmail.com>
2024-11-26 14:04:11 +0200

    ci: install nigthly version of rust

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt install -y nasm grub-pc-bin grub-common make mtools xorriso
 
+rustup install nightly-2024-10-14
 rustup default nightly-2024-10-14
 rustup override set nightly-2024-10-14
 rustup component add rust-src --toolchain nightly-2024-10-14-x86_64-unknown-linux-gnu
