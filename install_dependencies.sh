1a6366730efe13891e50e902a2a4b37829a0adf2
Mrgoblings <mrgoblings@gmail.com>
2024-11-26 14:09:52 +0200

    ci: output tustc version

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,4 +7,4 @@ rustup override set nightly-2024-10-14
 rustup target add x86_64-unknown-none --toolchain nightly
 rustup component add rust-src --toolchain nightly-2024-10-14-x86_64-unknown-linux-gnu
 
-rustc +nightly --version
+rustc --version
