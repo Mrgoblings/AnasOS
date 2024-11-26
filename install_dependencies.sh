481515702f784a273355e10008dfd8ed7251937d
Mrgoblings <mrgoblings@gmail.com>
2024-11-26 14:01:58 +0200

    ci: fix install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,9 @@
 sudo apt update
 sudo apt install -y nasm grub-pc-bin grub-common make mtools xorriso
-rustup update nightly
+
+rustup default nightly-2024-10-14
+rustup override set nightly-2024-10-14
 rustup target add x86_64-unknown-none --toolchain nightly
-rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+rustup component add rust-src --toolchain nightly-2024-10-14-x86_64-unknown-linux-gnu
 
 rustc +nightly --version
