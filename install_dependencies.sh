4d8ce2e6586d058000b60698f70c4f84d3a83c07
Mrgoblings <mrgoblings@gmail.com>
2024-11-26 13:52:22 +0200

    ci: debug pipeline

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt install -y nasm grub-pc-bin grub-common make mtools xorriso
-rustup toolchain install nightly-2024-10-14
-rustup override set nightly-2024-10-14
+rustup update nightly
 rustup target add x86_64-unknown-none --toolchain nightly
 rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+
+rustc +nightly --version
