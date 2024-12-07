e43ee8592a351f0b1806fb7890f59a5f2eca7aca
Mrgoblings <mrgoblings@gmail.com>
2024-12-07 13:37:18 +0200

    chore: include the memory allocator to the main

--- anasos-kernel/src/main.rs
+++ anasos-kernel/src/main.rs
@@ -7,7 +7,7 @@ use anasos_kernel::{
     println, 
     init, 
     hlt,
-    memory,
+    memory::{ self, BootInfoFrameAllocator}
 };
 use x86_64::{ structures::paging::Page, VirtAddr };
 
@@ -23,11 +23,17 @@ pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
     init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = memory::EmptyFrameAllocator;
-
+    
+    let mut mapper = unsafe { 
+        memory::init(phys_mem_offset) 
+    };
+    
+    let mut frame_allocator = unsafe { 
+        BootInfoFrameAllocator::init(&boot_info.memory_map) 
+    };
+    
     // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0));
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
     memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
     // write the string `New!` to the screen through the new mapping
