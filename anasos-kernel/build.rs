c66fea69b8f439efe65288c8e6549366cf30724c
Mrgoblings <mrgoblings@gmail.com>
2024-12-17 08:16:52 +0200

    perf: optimize build file readability

--- anasos-kernel/build.rs
+++ anasos-kernel/build.rs
@@ -1,6 +1,17 @@
 use std::env;
 use std::process::Command;
 
+fn assemble_file(input: &str, output: &str) {
+    let status = Command::new("nasm")
+        .args(&["-f", "elf64", input, "-o", output])
+        .status()
+        .expect(&format!("Failed to assemble {}", input));
+
+    if !status.success() {
+        panic!("Assembly of {} failed with status: {}", input, status);
+    }
+}
+
 fn main() {
     let target_dir: String = env::var("OUT_DIR").unwrap(); 
 
@@ -8,34 +19,9 @@ fn main() {
     let boot_64_o_path: String = format!("{}/boot-64.o", target_dir);
     let header_o_path = format!("{}/header.o", target_dir);
 
-
-    let status_boot = Command::new("nasm")
-        .args(&["-f", "elf64", "bootloader/boot.asm", "-o", &boot_o_path])
-        .status()
-        .expect("Failed to assemble boot.asm");
-
-    if !status_boot.success() {
-        panic!("Assembly of boot.asm failed with status: {}", status_boot);
-    }
-
-    let status_boot_64 = Command::new("nasm")
-        .args(&["-f", "elf64", "bootloader/boot-64.asm", "-o", &boot_64_o_path])
-        .status()
-        .expect("Failed to assemble boot-64.asm");
-
-    if !status_boot_64.success() {
-        panic!("Assembly of boot.asm failed with status: {}", status_boot_64);
-    }
-
-    let status_header = Command::new("nasm")
-        .args(&["-f", "elf64", "bootloader/header.asm", "-o", &header_o_path])
-        .status()
-        .expect("Failed to assemble header.asm");
-
-    if !status_header.success() {
-        panic!("Assembly of header.asm failed with status: {}", status_header);
-    }
-
+    assemble_file("bootloader/boot.asm", &boot_o_path);
+    assemble_file("bootloader/boot-64.asm", &boot_64_o_path);
+    assemble_file("bootloader/header.asm", &header_o_path);
 
     // Custom linker arguments
     println!("cargo:rustc-link-arg=-Tlinker.ld");
