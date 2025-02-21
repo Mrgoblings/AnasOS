ba1ac1939cab1e3773fd86170505a941208bc7a6
Mrgoblings <mrgoblings@gmail.com>
2025-02-21 02:16:56 +0200

    osfetch.rs

--- anasos-kernel/src/shell/osfetch.rs
+++ anasos-kernel/src/shell/osfetch.rs
@@ -7,6 +7,6 @@ pub(crate) struct OsFetch {}
 
 impl Command for OsFetch {
     fn execute(&self, _args: String) -> String {
-        "AnasOS 0.1.0".to_string()
+        "AnasOS 0.1.0\n".to_string()
     }
 }
